92a43ef537fe600336855f4b3012e52c62245e43
CiudadAlex <ciudad.alex@gmail.com>
2024-08-09 01:53:22 +0200

    InformationRetriever

--- PubmedSetup.py
+++ PubmedSetup.py
@@ -7,14 +7,16 @@ from bigxml import Parser, xml_handle_element, xml_handle_text
 from bigxml import BigXmlError
 from bigxml import HandlerTypeHelper, Streamable, XMLElement, XMLElementAttributes, XMLText
 
-#"PubmedArticleSet", "PubmedArticle", "PubmedData", "ArticleIdList", "ArticleId"
-#IdType="doi"
-#node.attributes["href"]
-
 @xml_handle_element("PubmedArticleSet", "PubmedArticle")
 def handler_pubmed_article(node):
     yield '-----------------------'
-    yield from node.iter_from(handler_article)
+    yield from node.iter_from(handler_doi, handler_article)
+
+@xml_handle_element("PubmedData", "ArticleIdList", "ArticleId")
+def handler_doi(node):
+
+    if node.attributes["IdType"] == "doi":
+        yield "DOI: " + node.text  # node content as a str
 
 @xml_handle_element("MedlineCitation", "Article")
 def handler_article(node):
@@ -29,12 +31,16 @@ def handler_abstract(node):
     yield "Abstract: " + node.text  # node content as a str
 
 
+def generateSeparatedFilesWithXml(file_path):
 
-with open("C:/Alex/Dev/data_corpus/InformationRetrieval/pubmed22n1513.xml", "rb") as f:
-    count = 0
-    for item in Parser(f).iter_from(handler_pubmed_article):
+    with open(file_path, "rb") as f:
+        count = 0
+        for item in Parser(f).iter_from(handler_pubmed_article):
 
-        if count == 100:
-            break
-        print(item)
-        count = count + 1
+            if count == 100:
+                break
+            print(item)
+            count = count + 1
+
+generateSeparatedFilesWithXml("C:/Alex/Dev/data_corpus/InformationRetrieval/pubmed22n1513.xml")
+
