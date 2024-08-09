e88f002fe6f742ab17df2ade1d4ea361390ac103
CiudadAlex <ciudad.alex@gmail.com>
2024-08-09 02:08:18 +0200

    InformationRetriever

--- PubmedSetup.py
+++ PubmedSetup.py
@@ -7,10 +7,14 @@ from bigxml import Parser, xml_handle_element, xml_handle_text
 from bigxml import BigXmlError
 from bigxml import HandlerTypeHelper, Streamable, XMLElement, XMLElementAttributes, XMLText
 
+START_ARTICLE_SYMBOL = "{{START_ARTICLE_SYMBOL}}"
+END_ARTICLE_SYMBOL = "{{END_ARTICLE_SYMBOL}}"
+
 @xml_handle_element("PubmedArticleSet", "PubmedArticle")
 def handler_pubmed_article(node):
-    yield '-----------------------'
+    yield START_ARTICLE_SYMBOL
     yield from node.iter_from(handler_doi, handler_article)
+    yield END_ARTICLE_SYMBOL
 
 @xml_handle_element("PubmedData", "ArticleIdList", "ArticleId")
 def handler_doi(node):
@@ -31,16 +35,29 @@ def handler_abstract(node):
     yield "Abstract: " + node.text  # node content as a str
 
 
-def generateSeparatedFilesWithXml(file_path):
+def generate_separated_files_with_xml(file_path):
 
     with open(file_path, "rb") as f:
         count = 0
+        list_items_doc = []
         for item in Parser(f).iter_from(handler_pubmed_article):
 
-            if count == 100:
-                break
-            print(item)
-            count = count + 1
+            if item == START_ARTICLE_SYMBOL:
+                list_items_doc.clear()
+            elif item == END_ARTICLE_SYMBOL:
+                generate_file(list_items_doc)
+                list_items_doc.clear()
 
-generateSeparatedFilesWithXml("C:/Alex/Dev/data_corpus/InformationRetrieval/pubmed22n1513.xml")
+                count = count + 1
+                if count == 100:
+                    break
+
+            else:
+                list_items_doc.append(item)
+
+def generate_file(list_items_doc):
+    print("--------------------------------------")
+    print(list_items_doc)
+
+generate_separated_files_with_xml("C:/Alex/Dev/data_corpus/InformationRetrieval/pubmed22n1513.xml")
 
