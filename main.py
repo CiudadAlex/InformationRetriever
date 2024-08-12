82238617fa2c1ce68d40d27e1f19afb553b1b4d7
CiudadAlex <ciudad.alex@gmail.com>
2024-08-13 00:21:27 +0200

    InformationRetriever

--- main.py
+++ main.py
@@ -15,6 +15,9 @@
 # Can choose any text generation model found here: https://huggingface.co/models?pipeline_tag=text-generation&sort=trending
 # Recommended to choose models from TheBloke who converts to GGUF format to run in limited memory
 !huggingface-cli download TheBloke/Llama-2-7B-GGUF llama-2-7b.Q4_K_M.gguf --local-dir models/ --local-dir-use-symlinks False
+
+# Install:
+https://learn.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-170
 """
 
 import PubmedSetup
@@ -25,7 +28,7 @@ file_path = base_dir
 output_dir_path = base_dir + "/processed"
 # PubmedSetup.generate_separated_files_of_xml_in_dir(file_path, output_dir_path)
 
-db = CustomDatasetProcessor.generate_vector_database_with_files(output_dir_path, chunk_size=1000, chunk_overlap=150)
+db = CustomDatasetProcessor.generate_vector_database_with_files(output_dir_path, chunk_size=1000, chunk_overlap=250)
 question = "Does Airflow have audit logs?"
 searchDocs = db.similarity_search(question)
 print(searchDocs[0].page_content)
