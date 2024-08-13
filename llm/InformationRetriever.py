51911f238c7b3dc948e032ab0ae5d8dcc1d62da7
CiudadAlex <ciudad.alex@gmail.com>
2024-08-14 01:33:23 +0200

    InformationRetriever

--- llm/InformationRetriever.py
+++ llm/InformationRetriever.py
@@ -6,53 +6,52 @@ from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 
 
-def build_llm(model_llama_ccp_path):
+class InformationRetriever:
 
-    # Callbacks support token-wise streaming
-    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+    def __init__(self, model_llama_ccp_path):
 
-    # Make sure the model path is correct for your system!
-    llm = LlamaCpp(
-        model_path=model_llama_ccp_path,
-        temperature=0.75,
-        max_tokens=2000,
-        top_p=1,
-        callback_manager=callback_manager,
-        verbose=True,  # Verbose is required to pass to the callback manager
-        n_ctx=2048
-    )
+        # Callbacks support token-wise streaming
+        callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 
-    return llm
+        # Make sure the model path is correct for your system!
+        self.llm = LlamaCpp(
+            model_path=model_llama_ccp_path,
+            temperature=0.75,
+            max_tokens=2000,
+            top_p=1,
+            callback_manager=callback_manager,
+            verbose=True,  # Verbose is required to pass to the callback manager
+            n_ctx=2048
+        )
 
+    def get_answer(self, question):
+        return self.llm(question)
 
-def get_answer(db, question, model_llama_ccp_path):
+    def get_answer_with_context(self, db, question):
 
-    llm = build_llm(model_llama_ccp_path)
+        # RAG prompt
+        template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer.
+        Context: {context}
+        Question: {question}
+        Helpful Answer:"""
+        qa_chain_prompt = PromptTemplate.from_template(template)
 
-    # Baseline answer from LLM without retrieval
-    llm(question)
+        # Create a retriever object from the 'db' with a search configuration where it retrieves up to 4 relevant
+        # splits/documents.
+        retriever = db.as_retriever(search_kwargs={"k": 2})
 
-    # RAG prompt
-    template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer.
-    Context: {context}
-    Question: {question}
-    Helpful Answer:"""
-    qa_chain_prompt = PromptTemplate.from_template(template)
+        # Create a question-answering instance (qa) using the RetrievalQA class.
+        # It's configured with a language model (llm), a chain type "refine", the retriever we created,
+        # and an option to not return source documents.
+        qa = RetrievalQA.from_chain_type(
+            llm=self.llm,
+            chain_type="stuff",
+            retriever=retriever,
+            return_source_documents=False,
+            verbose=True,
+            chain_type_kwargs={"prompt": qa_chain_prompt, "verbose": True},
+        )
 
-    # Create a retriever object from the 'db' with a search configuration where it retrieves up to 4 relevant splits/documents.
-    retriever = db.as_retriever(search_kwargs={"k": 2})
-
-    # Create a question-answering instance (qa) using the RetrievalQA class.
-    # It's configured with a language model (llm), a chain type "refine", the retriever we created, and an option to not return source documents.
-    qa = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=retriever,
-        return_source_documents=False,
-        verbose=True,
-        chain_type_kwargs={"prompt": qa_chain_prompt, "verbose": True},
-    )
-
-    result = qa({"query": question})
-    print(result["result"])
+        result = qa({"query": question})
+        print(result["result"])
 
--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 """
-
-# llama-spp-python library may require different ARGS depending on where you are installing it
-# Check https://python.langchain.com/docs/integrations/llms/llamacpp for installation options
-# Below is for Apple silicon
-#!CMAKE_ARGS="-DLLAMA_METAL=on" pip install -q llama-cpp-python
-!pip install -q llama-cpp-python
-
 # Big download of the model ~4Gb
-# Can choose any text generation model found here: https://huggingface.co/models?pipeline_tag=text-generation&sort=trending
-# Recommended to choose models from TheBloke who converts to GGUF format to run in limited memory
 
 pip install -U "huggingface_hub[cli]"
 huggingface-cli download TheBloke/Llama-2-7B-GGUF llama-2-7b.Q4_K_M.gguf --local-dir models/ --local-dir-use-symlinks False
@@ -16,14 +7,15 @@ huggingface-cli download TheBloke/Llama-2-7B-GGUF llama-2-7b.Q4_K_M.gguf --local
 or download in:
 https://huggingface.co/TheBloke/Llama-2-7B-GGUF/blob/main/llama-2-7b.Q4_K_M.gguf
 
-# Install:
+# Install Visual Studio for libraries:
+
 https://learn.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-170
 https://visualstudio.microsoft.com/es/vs/preview/
 """
 
 from custom_dataset_processors.pubmed import PubmedDatasetProcessor
 from vector_database import VectorDatabaseBuilder
-from llm import InformationRetriever
+from llm.InformationRetriever import InformationRetriever
 
 preprocess_dataset = False
 base_dir = "C:/Alex/Dev/data_corpus/InformationRetrieval"
@@ -41,7 +33,15 @@ print(str(len(searchDocs)))
 print(searchDocs[0].page_content)
 print("_______________________________________________")
 model_llama_ccp_path = "C:/Alex/Dev/models/llm/llama-2-7b.Q4_K_M.gguf"
-InformationRetriever.get_answer(db, question, model_llama_ccp_path)
+informationRetriever = InformationRetriever(model_llama_ccp_path)
+
+answer_raw = informationRetriever.get_answer(question)
+answer_context = informationRetriever.get_answer_with_context(db, question)
+
+print("##############################################################################################################")
+print(answer_raw)
+print("##############################################################################################################")
+print(answer_context)
 
 
 
